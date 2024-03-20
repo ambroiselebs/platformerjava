89919aaacc6d468ae185554e9fa77641077e5f29
ambroiselebs <amberenger06@gmail.com>
2024-03-20 09:35:47 +0100

    Player movements (Keyboard+mouse)

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -3,11 +3,14 @@ package main;
 import javax.swing.*;
 
 public class GameWindow extends JFrame {
-    public GameWindow() {
+    public GameWindow(GamePanel gamePanel) {
         setSize(500, 400);
         setResizable(false);
         setTitle("My Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        add(gamePanel);
 
         setVisible(true);
     }
